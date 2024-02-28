cee3149031b6706c60ace89508cee46ca3a98f6d
lai2-24 <lai2-24@itismeucci.com>
2024-02-28 09:23:24 +0000

    finire input dinamici

--- 2way-binding/src/app/app.component.ts
+++ 2way-binding/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Person } from './model/person';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,17 +15,52 @@ export class AppComponent {
   title1 = ''
   title = 'unclepear'
   num = 0
-  buttonDisabled: boolean = false
+  buttonDisabled: boolean = true
+
+  person2: Person = {
+
+    name:"gigu", surname: "kriks", age:"12"
+  }
+  person: Person = new Person()
+
+  people: Person[] = [
+
+   /*  {name: "frango", surname:"frengo", age:"18"},
+    {name: "pluto", surname:"pippo", age:"44"} */
+  ]
+  
 
   clickButton = () => {
     this.num++
-    if(this.num > 0)
-      this.buttonDisabled = true
+   /*  if(this.num > 0)
+      this.buttonDisabled = true */
   }
 
-  changeTitle(){
+  changeTitle = () =>{
 
     this.title = this.title1;
+  }
+
+  unlockButton(){
+
+    if(this.person.name != "")
+      if(this.person.surname != "")
+        if(this.person.age != "")
+          this.buttonDisabled = false
+  }
+
+  addPerson(){
+    
+    this.people.push(this.person)
+  }
+
+  editPerson(){
+
+  }
+
+  deletePerson(){
+
+
   }
 
 }
---
+++ 2way-binding/src/app/model/person.ts
@@ -0,0 +1,14 @@
+export class Person {
+
+    name: string
+    surname: string
+    age: string
+
+    constructor() {
+
+        this.name = ""
+        this.surname = ""
+        this.age = "" 
+    }
+}
+
