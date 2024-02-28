342efb7b1d5524a7c186e9a9924bf664fc398ded
lai2-24 <lai2-24@itismeucci.com>
2024-02-28 08:14:48 +0000

    input 2way-binding

--- 2way-binding/src/app/app.component.ts
+++ 2way-binding/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  title1 = ''
   title = 'unclepear'
   num = 0
   buttonDisabled: boolean = false
@@ -17,6 +19,11 @@ export class AppComponent {
     this.num++
     if(this.num > 0)
       this.buttonDisabled = true
+  }
+
+  changeTitle(){
+
+    this.title = this.title1;
   }
 
 }
